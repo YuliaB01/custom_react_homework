28cde7c672d5a200630944f10768ef3aee64c517
Yulia Bilash <starlight05032015@gmail.com>
2019-10-17 23:15:23 +0300

    Added some fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,52 +1,83 @@
 const CustomReact = {
     createElement: function (type, props, children) {
-        let element = document.createElement(type);
+        let element;
 
-        if (children && typeof children === 'string') {
-            element.innerHTML = children;
+        if (type && typeof type === 'string') {
+            element = document.createElement(type);
+        } else {
+            throw Error('Element type is not valid!');
         }
 
-        if (children && typeof children === 'object') {
-            children.forEach(child => {
-                if (typeof child === 'string') {
-                    let textNode = document.createTextNode(child);
-                    element.appendChild(textNode);
-                } else {
-                    element.appendChild(child);
-                }
-            });
-        }
+        this.appendChildren(element, children);
 
-        if (props) {
-            for (let key in props) {
-                if (props.hasOwnProperty(key)) {
-                    if (typeof props[key] === 'object') {
-                        for (let i in props[key]) {
-                            if (props[key].hasOwnProperty(i)) {
-                                element[key][i] = props[key][i];
-                            }
-                        }
-                    } else {
-                        element[key] = props[key];
-                    }
-                }
-            }
-        }
+        this.applyProps(element, props);
 
         return element;
     },
 
     render: function (elem, appendTo) {
-        if (!this.isValidContainer(appendTo)) {
+        if (!this.isContainerValid(appendTo)) {
             throw Error('Container is not valid!');
         } else {
             appendTo.appendChild(elem);
         }
     },
 
-    isValidContainer: function (node) {
+    isContainerValid: function (node) {
         const ELEMENT_NODE = 1;
-        const DOCUMENT_NODE = 9;
-        return !!(node && (node.nodeType === ELEMENT_NODE || node.nodeType === DOCUMENT_NODE));
+
+        if (node && node.nodeType === ELEMENT_NODE) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    applyProps: function (element, props) {
+        if (props) {
+            if (typeof props !== 'object') {
+                throw Error('Props type is not valid. Props must be an object.');
+            }
+
+            let keys = Object.keys(props);
+            for (let key of keys) {
+                if (typeof props[key] === 'object') {
+                    let objKeys = Object.keys(props[key]);
+
+                    for (let i of objKeys) {
+                        element[key][i] = props[key][i];
+                    }
+                } else {
+                    element[key] = props[key];
+                }
+            }
+        }
+    },
+
+    appendChildren: function (element, children) {
+        if (children) {
+            if (typeof children !== 'string' && typeof children !== 'object') {
+                throw Error('Argument "children" is not valid');
+            }
+
+            if (typeof children === 'string') {
+                element.innerHTML = children;
+            }
+
+            if (typeof children === 'object') {
+                if (!children.length) {
+                    element.appendChild(children);
+                } else {
+                    children.map(child => {
+                        if (typeof child === 'string') {
+                            let textNode = document.createTextNode(child);
+                            element.appendChild(textNode);
+                        } else {
+                            element.appendChild(child);
+                        }
+                    });
+                }
+            }
+        }
     }
 };
--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,15 @@ const app = CustomReact.createElement(
     'div',
     { style: { backgroundColor: '#ff9000' }, className: 'contentWrapper' },
     [
-        CustomReact.createElement('span', undefined, 'Hello world!'),
+        CustomReact.createElement('h1', undefined, 'Hello world!'),
         CustomReact.createElement('br'),
         'This is just a text node',
-        CustomReact.createElement('div', { textContent: 'Text content' })
+        CustomReact.createElement('div', { textContent: 'Text content' }),
+        CustomReact.createElement(
+            'div',
+            {className: 'imageWrapper'},
+            CustomReact.createElement('img', { id: 'image', src: 'http://placehold.it/200x200' })
+        )
     ]
 );
 CustomReact.render(app, document.getElementById('root'));
